1c1cdf00a41da735bb23b3834193f83269f13ce9
Raboro <raborogit@gmail.com>
2022-05-25 11:29:25 +0200

    cars_in_slots

---
+++ Round 1/task 01/get_data.py
@@ -0,0 +1,16 @@
+from itertools import takewhile
+
+class GetData():
+
+    @staticmethod
+    def get_and_return_data():
+        with open("parkplatz0.txt", "r") as file:
+            data = file.read().split("\n")
+        
+        data.pop(-1)
+
+        ALP = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        break_index = [index+1 for index, char in enumerate(ALP) if char == data[0][2]]
+        cars_in_slots = [char for char in takewhile(lambda char: char != ALP[break_index[0]], ALP)]
+
+        print(cars_in_slots)
--- Round 1/task 01/main.py
+++ Round 1/task 01/main.py
@@ -1,4 +1,8 @@
+from get_data import GetData
+
+def main() -> None:
+    GetData.get_and_return_data()
+
+
 if __name__ == "__main__":
-    print("hi")
-    print("hello")
-    print("test")
+    main()
